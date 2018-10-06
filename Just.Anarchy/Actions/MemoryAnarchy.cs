71b3173a13e8e1fc07a2ecf086f7e81a097e01d8
Matt Jenner <jenner_m@outlook.com>
2018-10-06 18:32:21 +0100

    Updated MemoryAnarchy to use revised memory consumption code, much faster only uses one pointer now.

--- Just.Anarchy/Actions/MemoryAnarchy.cs
+++ Just.Anarchy/Actions/MemoryAnarchy.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Just.Anarchy.Actions
 {
-    public class MemoryAnarchy : ICauseAnarchy
+    public class MemoryAnarchy : ICauseAnarchy, ICauseScheduledAnarchy
     {
         public CauseAnarchyType AnarchyType { get; set; } = CauseAnarchyType.Passive;
         public bool Active { get; set; } = false;
 
         public int StatusCode => 0;
 
-        public string Body => string.Empty;
+        // TODO: AmountMb needs to be parameterized.
+        private const int AmountMb = 1024;
+        private const int Megabyte = 1048576;
 
-        public async Task ExecuteAsync()
+        public string Body => string.Empty;
+        public Task ExecuteAsync(TimeSpan? duration, CancellationToken cancellationToken)
         {
-            var rnd = new Random();
-            var list = new List<byte[]>();
-            var limit = rnd.Next(1000, 1000000);
-            var result = Parallel.ForEach(Enumerable.Range(1, limit),
-                new ParallelOptions { MaxDegreeOfParallelism = 4 },
-                num =>
-                {
-                    byte[] b = new byte[1024];
-                    b[rnd.Next(0, b.Length)] = byte.MaxValue;
-                    list.Add(b);
-                }
-            );
+            return Task.Run(async () =>
+            {
+                var totalSize = Megabyte * AmountMb;
+
+                // Consume memory quickly - Unmanaged seems most consistent
+                var pointer = Marshal.AllocHGlobal(Marshal.SizeOf<byte>() * totalSize);
+                Parallel.For(0, totalSize,
+                    new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                    i => Marshal.WriteByte(pointer, i, new byte())
+                );
+
+                // Hold the memory for the allotted duration (or 1 min)
+                await Task.Delay(duration ?? TimeSpan.FromMinutes(1), cancellationToken);
+                
+                // Release Memory
+                Marshal.FreeHGlobal(pointer);
+            }, cancellationToken);
+
         }
     }
 }
