33db4a0755ad91178a2279d36e012cfab2dfc110
Matt Jenner <jenner_m@outlook.com>
2018-10-06 18:35:54 +0100

    Made anarchy actions cancellable and take a duration (to be used if applicable).

--- Just.Anarchy/Actions/RandomErrorResponseAnarchy.cs
+++ Just.Anarchy/Actions/RandomErrorResponseAnarchy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -15,27 +16,31 @@ namespace Just.Anarchy.Actions
 
         public string Body { get; set; }
 
-        public Task ExecuteAsync()
+        public Task ExecuteAsync(TimeSpan? duration, CancellationToken cancellationToken)
         {
-            Random random = new Random();
+            var random = new Random();
             StatusCode = random.Next(400, 600);
-            Body = JsonConvert.SerializeObject(RandomObject());
+            Body = JsonConvert.SerializeObject(RandomObject(cancellationToken));
             return Task.FromResult(string.Empty);
         }
 
-        private static Dictionary<string,object> RandomObject()
+        private static Dictionary<string,object> RandomObject(CancellationToken cancellationToken)
         {
-            Random random = new Random();
-            Dictionary<string, object> obj = new Dictionary<string, object>();
-            Parallel.ForEach(Enumerable.Range(0,1000), x => {
-                obj.Add(RandomString(random.Next(1, 1000)), RandomString(random.Next(1, 1000)));
-            });
+            var random = new Random();
+            var obj = new Dictionary<string, object>();
+            Parallel.ForEach(Enumerable.Range(0,1000)
+                .AsParallel()
+                .WithCancellation(cancellationToken), 
+                x => {
+                    obj.Add(RandomString(random.Next(1, 1000)), RandomString(random.Next(1, 1000)));
+                }
+                );
             return obj;
         }
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            Random random = new Random();
+            var random = new Random();
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
