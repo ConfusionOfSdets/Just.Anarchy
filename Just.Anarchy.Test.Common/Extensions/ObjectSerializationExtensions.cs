66301c3b1a645c0bb8b0c2deabebdc28db305d2f
Matt Jenner <jenner_m@outlook.com>
2018-10-29 07:38:51 +0000

    Added Common project to share utils and extensions across to integration project.

---
+++ Just.Anarchy.Test.Common/Extensions/ObjectSerializationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace Just.Anarchy.Test.Common.Utilities
+{
+    public static class ObjectSerializationExtensions
+    {
+        public static string ToEscapedString(this object incoming) => Regex.Escape(incoming.Serialise()).Replace("\"","\\");
+
+        public static string Serialise(this object incoming) => JsonConvert.SerializeObject(incoming);
+    }
+}
