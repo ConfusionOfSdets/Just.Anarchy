eb1c16428baa338532684f76fbd76f8c8fc8ebe7
Matt Jenner <jenner_m@outlook.com>
2018-11-17 22:15:52 +0000

    Added unit tests for AnarchyMiddleware

--- Just.Anarchy.Test.Unit/AnarchyMiddlewareTests.cs
+++ Just.Anarchy.Test.Unit/AnarchyMiddlewareTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System.Threading.Tasks;
+using Just.Anarchy.Core.Interfaces;
+using Just.Anarchy.Test.Common.Builders;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using NUnit.Framework;
 using NUnit.Framework.Internal;
 
 namespace Just.Anarchy.Test.Unit
@@ -6,7 +11,79 @@ namespace Just.Anarchy.Test.Unit
     [TestFixture]
     public class AnarchyMiddlewareTests
     {
-        public void 
-        
+        [Test]
+        public async Task Invoke_TriggersAnarchyManager_ForNonAnarchyRoutes()
+        {
+            // Arrange
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            var exceptionHandler = Substitute.For<IHandleAnarchyExceptions>();
+            var next = Substitute.For<RequestDelegate>();
+            var context = Get.CustomBuilderFor.MockHttpContext.WithPath("/foo").Build();
+            var sut = new AnarchyMiddleware(next, anarchyManager, exceptionHandler);
+
+            // Act
+            await sut.Invoke(context);
+
+            // Assert
+            await anarchyManager.Received(1).HandleRequest(context, next);
+        }
+
+        [Test]
+        public async Task Invoke_DoesNotTriggerAnarchyManager_ForAnarchyRoutes()
+        {
+            // Arrange
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            var exceptionHandler = Substitute.For<IHandleAnarchyExceptions>();
+            var next = Substitute.For<RequestDelegate>();
+            var context = Get.CustomBuilderFor.MockHttpContext.WithPath("/anarchy/foo").Build();
+            var sut = new AnarchyMiddleware(next, anarchyManager, exceptionHandler);
+
+            // Act
+            await sut.Invoke(context);
+
+            // Assert
+            await anarchyManager.DidNotReceive().HandleRequest(context, next);
+        }
+
+        [Test]
+        public async Task Invoke_CallsNext_ForNonAnarchyRoutes_ThatDoNotAffectResponse()
+        {
+            // Arrange
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            anarchyManager
+                .HandleRequest(Arg.Any<HttpContext>(), Arg.Any<RequestDelegate>())
+                .Returns(Task.FromResult(false));
+            var exceptionHandler = Substitute.For<IHandleAnarchyExceptions>();
+            var next = Substitute.For<RequestDelegate>();
+            var context = Get.CustomBuilderFor.MockHttpContext.WithPath("/foo").Build();
+            var sut = new AnarchyMiddleware(next, anarchyManager, exceptionHandler);
+
+            // Act
+            await sut.Invoke(context);
+
+            // Assert
+            await next.Received(1).Invoke(context);
+        }
+
+        [Test]
+        public async Task Invoke_DoesNotCallNext_ForNonAnarchyRoutes_ThatAffectResponse()
+        {
+            // Arrange
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            anarchyManager
+                .HandleRequest(Arg.Any<HttpContext>(), Arg.Any<RequestDelegate>())
+                .Returns(Task.FromResult(true));
+            var exceptionHandler = Substitute.For<IHandleAnarchyExceptions>();
+            var next = Substitute.For<RequestDelegate>();
+            var context = Get.CustomBuilderFor.MockHttpContext.WithPath("/foo").Build();
+            var sut = new AnarchyMiddleware(next, anarchyManager, exceptionHandler);
+
+            // Act
+            await sut.Invoke(context);
+
+            // Assert
+            await next.DidNotReceive().Invoke(context);
+        }
+
     }
 }
