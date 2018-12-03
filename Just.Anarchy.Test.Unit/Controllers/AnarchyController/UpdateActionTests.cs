a2047f6ba16e0d21fece4bd798250e0b61777501
Matt Jenner <jenner_m@outlook.com>
2018-12-03 08:22:45 +0000

    - Controller tests for UpdateAction. - AnarchyManager tests for UpdateAction.

---
+++ Just.Anarchy.Test.Unit/Controllers/AnarchyController/UpdateActionTests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Just.Anarchy.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Just.Anarchy.Test.Unit.Controllers.AnarchyController
+{
+    [TestFixture]
+    public class UpdateActionTests : BaseControllerTests
+    {
+        const string updatePayload = "fake request body";
+        const string actionType = "aFakeAnarchyType";
+
+        [Test]
+        public async Task UpdateAction_ReturnsOkResult()
+        {
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            var sut = ControllerWithContextBuilder(() => new Anarchy.Controllers.AnarchyController(anarchyManager));
+            sut.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(updatePayload));
+
+            //Act
+            var result = await sut.UpdateAction(actionType);
+
+            //Assert
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [Test]
+        public async Task UpdateAction_PassesPayloadToService()
+        {
+            //arrange
+            var anarchyManager = Substitute.For<IAnarchyManagerNew>();
+            var sut = ControllerWithContextBuilder(() => new Anarchy.Controllers.AnarchyController(anarchyManager));
+            sut.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(updatePayload));
+
+            //Act
+            await sut.UpdateAction(actionType);
+
+            //Assert
+            anarchyManager.Received(1).UpdateAction(actionType, updatePayload);
+        }
+    }
+}
---
+++ Just.Anarchy.Test.Unit/Core/AnarchyManager/UpdateActionTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Just.Anarchy.Core;
+using Just.Anarchy.Core.Interfaces;
+using Just.Anarchy.Exceptions;
+using Just.Anarchy.Test.Common.Builders;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Just.Anarchy.Test.Unit.Core.AnarchyManager
+{
+    [TestFixture]
+    public class UpdateActionTests
+    {
+        private const string FakeUpdatePayload = "afakepayload";
+
+        [Test]
+        [TestCase("TESTAnarchyType")]
+        [TestCase("testAnarchyType")]
+        public void MatchingOrchestrator(string anarchyType)
+        {
+            //arrange
+            var orchestrator = Get.MotherFor.MockAnarchyActionOrchestrator
+                .OrchestratorWithScheduleNamed("testAnarchyType").Build();
+            var sut = new AnarchyManagerNew(new [] { orchestrator });
+
+
+            //act
+            sut.UpdateAction(anarchyType, FakeUpdatePayload);
+
+            //assert
+            orchestrator.Received(1).UpdateAction(FakeUpdatePayload);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("    ")]
+        [TestCase("")]
+        [TestCase("wontMatch")]
+        public void NoMatchingOrchestrator(string anarchyType)
+        {
+            //arrange
+            var schedule = new Schedule();
+            var orchestrator = Substitute.For<IActionOrchestrator>();
+            var action = Substitute.For<ICauseAnarchy>();
+            action.Name.Returns("testAnarchyType");
+            orchestrator.AnarchyAction.Returns(action);
+            var sut = new AnarchyManagerNew(new [] { orchestrator });
+
+            //act
+            var exception = Assert.Catch(() => sut.UpdateAction(anarchyType, FakeUpdatePayload));
+
+            //assert
+            exception.Should().BeOfType<AnarchyActionNotFoundException>();
+            orchestrator.Received(0).UpdateAction(Arg.Any<string>());
+        }
+
+        [Test]
+        [TestCase("firstActionName", "secondActionName", 1, 0)]
+        [TestCase("secondActionName", "firstActionName", 0, 1)]
+        [TestCase("firstActionName", "firstActionName", 1, 0)]
+        public void SelectsFirstMatchingOrchestrator(string firstActionName, string secondActionName, int firstCount, int secondCount)
+        {
+            //arrange
+            var orchestrator1 = Get.MotherFor
+                .MockAnarchyActionOrchestrator
+                .OrchestratorWithScheduleNamed(firstActionName).Build();
+            var orchestrator2 = Get.MotherFor
+                .MockAnarchyActionOrchestrator
+                .OrchestratorWithScheduleNamed(secondActionName).Build();
+            var sut = new AnarchyManagerNew(new [] { orchestrator1, orchestrator2 });
+
+            //act
+            sut.UpdateAction("firstActionName", FakeUpdatePayload);
+
+            //assert
+            orchestrator1.Received(firstCount).UpdateAction(FakeUpdatePayload);
+            orchestrator2.Received(secondCount).UpdateAction(FakeUpdatePayload);
+        }
+    }
+}
